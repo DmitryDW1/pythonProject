eb367264ad358b988c87247d713ab60bb79a4f5f
DmitryDW1 <DmitryDW13@yandex.ru>
2023-12-20 18:18:47 +0600

    Add task_9

--- seminar_1/task_9.py
+++ seminar_1/task_9.py
@@ -8,6 +8,6 @@ for k in [0, 4]:
     for i in range(2, 11):
         res = ''
         for j in range(2 + k, 6 + k):
-            res += f'{i:^2} x {j:^2} = {i * j:^2}\t'
+            res += f'{j:^2} x {i:^2} = {i * j:^2}\t'
         print(res)
     print()
