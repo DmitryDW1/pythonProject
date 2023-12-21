cbe7c0aa001401bd6a3decfe21f920735cdca037
DmitryDW1 <DmitryDW13@yandex.ru>
2023-12-21 13:12:06 +0600

    Add task_dig_6.py and task_dig_7.py, task_dig_8.py

---
+++ Lection2/task_dig_6.py
@@ -0,0 +1,4 @@
+DEFAULT = 42
+num = int(input("Input number or 0: "))
+level = num or DEFAULT
+print(level)
---
+++ Lection2/task_dig_7.py
@@ -0,0 +1,5 @@
+name = input("Name? ")
+if name:
+    print("Hello, " + name)
+else:
+    print("Hello, anonimus")
