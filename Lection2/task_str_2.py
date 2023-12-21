1797c73d975653e5797bb0fd2c62f39f325226e6
DmitryDW1 <DmitryDW13@yandex.ru>
2023-12-21 13:25:19 +0600

    Add task_str_1.py.py and task_str_2.py.py, task_str_3.py.py

---
+++ Lection2/task_str_2.py
@@ -0,0 +1,7 @@
+text = 'Привет.' 'Как ты, друг?' 'Рад тебя видеть.' # Плохой код
+print(text)
+
+very_long_text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias animi assumenda at aut ' \
+'commodi, consequatur cumque ea harum, hic idillum ipsam itaque laboriosam magnam minus nam nulla ' \
+'numquam obcaecati officia officiis porro possimus praesentium quaerat temporibus ullam veniam? '
+print(very_long_text)
