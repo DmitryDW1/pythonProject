775eca2180ed629a30e61629975122d5a28154a0
DmitryDW1 <DmitryDW13@yandex.ru>
2023-12-25 19:14:59 +0600

    Add home_work_sem_3

---
+++ seminar_3/home_work_sem_3/hw_task_1.py
@@ -0,0 +1,33 @@
+"""
+На вход подается словарь со списком вещей для похода в качестве ключа и их массой max_weight в качестве значения.
+
+Определите какие вещи влезут в рюкзак backpack передав его максимальную грузоподъёмность.
+Предметы не должны дублироваться.
+
+Результат должен быть в виде словаря {предмет:вес} с вещами в рюкзаке и сохранен в переменную backpack.
+
+Достаточно получить один допустимый вариант и сохранить в переменную backpack. Не выводите backpack на экран
+"""
+import decimal
+from decimal import Decimal
+
+items = {
+    "ключи": 0.3,
+    "кошелек": 0.3,
+    "телефон": 0.8,
+    "зажигалка": 0.1
+}
+max_weight = 1.0
+
+# Введите ваше решение ниже
+
+max_weight_items = Decimal(str(max_weight))
+backpack = {}
+for i in items:
+    if max_weight_items >= items[i]:
+        if i not in backpack:
+            backpack.setdefault(i, items[i])
+        max_weight_items -= Decimal(str(items.get(i)))
+    else:
+        continue
+print(backpack)
