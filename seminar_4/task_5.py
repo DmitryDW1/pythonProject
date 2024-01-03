83f54e761a5d099311492475198dc90b43ff6c0e
DmitryDW <DmitryDW1@yandex.ru>
2024-01-03 12:23:06 +0600

    Add change seminar_4 task_5.py

--- seminar_4/task_5.py
+++ seminar_4/task_5.py
@@ -14,4 +14,12 @@ def bonus_calculation(names, base, bonus):
     return result
 
 
-print(bonus_calculation(['dima', 'maxim'], [2000, 4000], ['0.25', '0.33']))
+print(bonus_calculation(['dima', 'maxim'], [2000, 4000], ['0.25%', '0.33%']))
+
+
+def solution(names, salary, bonus):
+    data = list(zip(names, salary, bonus))
+    return {item[0]: item[1] * float(item[2].replace('%', '')) / 100 for item in data}
+
+
+print(solution(['dima', 'maxim'], [2000, 4000], ['0.25%', '0.33']))
